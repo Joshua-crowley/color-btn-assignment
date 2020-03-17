2ac02e692cf616acfa4b10b5c842e369f57b847c
Joshua Crowley <joshua.ginger.crowley@gmail.com>
2020-03-16 23:36:31 -0600

    added button creation and option creating

--- colors.js
+++ colors.js
@@ -1,1 +1,3 @@
-var colors = ['blue', 'purple', 'limegreen'];
+var colors = ['Green', 'Red', 'Yellow'];
+
+var listColors = ['Blue', 'Pink', 'Orange', 'Magenta', 'Teal', 'Aqua', 'Coral', 'Crimson', 'DarkSeaGreen', 'LimeGreen', 'OliveDrab', 'RebeccaBlue', 'SeaGreen', 'Violet', 'OrangeRed', 'GoldenRod', 'FireBrick', 'DeepPink', 'DarkRed', 'DarkSalmon',];
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var colorButton = document.getElementById('color-btns');
 var addBtn = document.getElementById('add-btn');
 var dropdown = document.getElementById('color-options');
 var removeBtn = document.getElementById('remove-current-btn');
@@ -19,32 +20,45 @@ removeBtn.addEventListener('click', function() {
 // Does everything necessary to setup our site for the first time
 function init() {
   // populate dropdown with color options
-  for (var i = 0; i < colors.length; i++) {
-    createColorOption(colors[i]);
+  for (var i = 0; i < listColors.length; i++) {
+    createColorOption(listColors[i]);
   }
   // generate the initial 3 color buttons
-  createColorButton('red');
-  createColorButton('yellow');
-  createColorButton('green');
+  createColorButton('Red');
+  createColorButton('Yellow');
+  createColorButton('Green');
 }
 
 // creates a new option of a given color and adds it to the dropdown
 function createColorOption(color) {
   // create an option
+  var item = document.createElement('option');
   // set the option's value to the given color
+  item.value = color;
   // set the option's inner text to be the given color
+  item.innerText = color;
   // add the option to the dropdown in the DOM
+  dropdown.appendChild(item);
 }
 
 // creates a new button of a given color and adds it to the color-btns container
 function createColorButton(color) {
   // create a button
+  var item = document.createElement('button');
   // set the button's text to say the color name
+  item.innerText = color;
   // set the style of the button to have a background color of the given color
+  item.style.background = color;
   // assign a button class
+  item.className = color;
   // assign an id of "<color-name-here>-btn" (replace the stuff in <>'s)
+  item.id = color + '-btn';
   // create an event listener on this button that sets the background color to be the given color
+  // color + '-btn'.addEventListener('click' function() {
+  //   body.style.backgroundColor = color;
+  // });
   // append into DOM
+  colorButton.append(item);
 }
 
 // remove an option of a given color from the dropdown
